894ca8ce7880aa333703436d6e038fb487e4601b
DAXILSAXENA <daxildaxil@gmail.com>
2021-06-07 14:22:24 +0530

    Added Daily Wage

--- EmployeeWageProblemJava.java
+++ EmployeeWageProblemJava.java
@@ -1,17 +1,23 @@
-// Learning Problem(LP) - Employee Wage Computation Problem Part1
+// Learning Problem(LP) - Employee Wage Computation Problem Part2
 
-// UC-1 Check Employee is Present or Absent
+// UC-2 Calculate Daily Employee Wage
 
-public class EmpWageBuilderUC1 {
+public class EmpWageBuilderUC2 {
 
 	public static void main(String[] args) {
 		// constants
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		// variables
+		int empHrs = 0;
+		int empWage = 0;
 			// computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
-		    System.out.println("Employee is Present");
+		    empHrs = 8;
 		else
-		    System.out.println("Employee is Absent");
+		    empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage: " + empWage);
 	}
 }
