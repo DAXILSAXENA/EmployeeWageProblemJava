9467be66edf7d688306ca84ab16db3e5cc6e43ab
DAXILSAXENA <daxildaxil@gmail.com>
2021-06-16 17:32:25 +0530

    Calculate Wages Till a Condition is Reached

--- EmployeeWageProblemJava.java
+++ EmployeeWageProblemJava.java
@@ -1,34 +1,37 @@
-public class EmpWageBuilderUC5 {
+public class EmpWageBuilderUC6 {
 
-                // Constants
-                public static final int IS_PART_TIME = 1;
-                public static final int IS_FULL_TIME = 2;
-                public static final int EMP_RATE_PER_HOUR = 20;
-                public static final int NUM_OF_WORKING_DAYS = 20;
+	// Constants
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
-                public static void main(String[] args) {
+	public static void main(String[] args) {
 
-                        // Variables
-                        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+		// Variables
+		int totalEmpHrs = 0, totalWorkingDays = 0, empHrs = 0;
 
-                        // Computations
-                        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-                                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-                                switch (empCheck) {
-                                        case IS_FULL_TIME:
-                                                empHrs = 8;
-                                                break;
-                                        case IS_PART_TIME:
-                                                empHrs = 4;
-                                                break;
-                                        default:
-                                                empHrs = 0;
-                                }
+		// Computations
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
 
-                        empWage = empHrs * EMP_RATE_PER_HOUR;
-                        totalEmpWage += empWage;
-                        System.out.println("Employee Wage : " + empWage);
-                }
-                System.out.println("Total Employee Wage : " + totalEmpWage);
-        }
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			default:
+				empHrs = 0;
+			}
+
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: " + totalWorkingDays + " Emp hr: " + empHrs);
+		}
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Employee Wage : " + totalEmpWage);
+	}
 }
