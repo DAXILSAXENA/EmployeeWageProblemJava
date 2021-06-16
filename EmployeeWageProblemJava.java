daca18e36188eac434b82afd21b861b6a20a7f86
DAXILSAXENA <daxildaxil@gmail.com>
2021-06-16 17:19:26 +0530

    Added Part time Employee & Wage

--- EmployeeWageProblemJava.java
+++ EmployeeWageProblemJava.java
@@ -1,23 +1,25 @@
-// Learning Problem(LP) - Employee Wage Computation Problem Part2
+public class EmpWageBuilderUC3 {
 
-// UC-2 Calculate Daily Employee Wage
+        public static void main(String[] args) {
 
-public class EmpWageBuilderUC2 {
 
-	public static void main(String[] args) {
-		// constants
-		int IS_FULL_TIME = 1;
-		int EMP_RATE_PER_HOUR = 20;
-		// variables
-		int empHrs = 0;
-		int empWage = 0;
-			// computation
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
-		    empHrs = 8;
-		else
-		    empHrs = 0;
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee Wage: " + empWage);
-	}
+        // Constants
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int EMP_RATE_PER_HOUR = 20;
+
+        // Variables
+        int empHrs = 0;
+        int empWage = 0;
+
+        // Computations
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+
+        if (empCheck == IS_FULL_TIME)
+                empHrs = 8;
+        else if (empCheck == IS_PART_TIME)
+                empHrs = 4;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage : " + empWage);
+        }
 }
