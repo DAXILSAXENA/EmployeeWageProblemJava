e231c45e9750ea17f91aaba2781bfacfa4787384
DAXILSAXENA <daxildaxil@gmail.com>
2021-06-16 17:38:24 +0530

    Added Ability to compute Emp Wage For Multiple Companies

--- EmployeeWageProblemJava.java
+++ EmployeeWageProblemJava.java
@@ -1,13 +1,13 @@
-public class EmpWageBuilderUC7 {
+public class EmpWageBuilderUC8 {
+	// Constants
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
 
-	public static int computeEmpWage() {
+	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		// Variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		// Computation
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
@@ -23,12 +23,13 @@ public class EmpWageBuilderUC7 {
 			totalEmpHrs += empHrs;
 			System.out.println("Day#: " + totalWorkingDays + " Emp Hrs: " + empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total Emp Wage: " + totalEmpWage);
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Total Emp Wage: for Company: " + company + " is: " + totalEmpWage);
 		return totalEmpWage;
 	}
 
 	public static void main(String[] args) {
-		computeEmpWage();
+		computeEmpWage("Dmart", 20, 20, 100);
+		computeEmpWage("Reliance", 10, 40, 200);
 	}
 }
